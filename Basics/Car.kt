cea8bcd6fec82016c608e88d0aa0a7170645c5c4
nneslihanyy <neslihanyagmurca@icloud.com>
2026-04-07 19:49:10 +0300

    refactor Car class: make year property private, add getYear and setYear functions with validation

--- Basics/Car.kt
+++ Basics/Car.kt
@@ -5,7 +5,7 @@ package com.example.kotlin.models
 class Car(
 var brand : String = "",
 var model : String = "",
-var year : Int = 0
+private var year : Int = 0
 ){
     // içi boş bir Car sınıfı oluşturduk
     //eğer erişim belirteci belirtilmediyse default olarak publictir, yani her yerden erişilebilir
@@ -30,6 +30,18 @@ var year : Int = 0
     fun rent(){
         println("The car is rented ${brand} ${model} ${year}")
     }
+
+    fun getYear(): Int {
+        return year
+    }
+    fun setYear(year: Int) {
+       if(year <1990 || year > 2026){
+        println("Invalid year")
+    }
+  else{this.year = year}
+        
+    
+    }
 }
 
 //class içi değişken manuel yönetilmelidir.
@@ -47,7 +59,17 @@ fun main() {
 
     var car1 : Car = Car("Toyota", "Camry", 2020) // Constructor (yapıcı) fonksiyonu, Car sınıfından bir nesne oluşturur
     car1.rent() // Car sınıfının rent fonksiyonunu çağırır, The car is rented  0 çıktısı verir çünkü brand, model ve year değerleri boş ve sıfır olarak atanmıştır
-    var car2 : Car2 = Car2("Honda", "Civic", 2019)
-    car2.rent() // The car is rented  0 çıktısı verir çünkü brand, model ve year değerleri boş ve sıfır olarak atanmıştır
+    println(car1.brand) // get >> Toyota çıktısı verir
+    car1.brand = "Honda" // set >> brand değerini Honda olarak değiştirir
+    println(car1.brand) // get >> Honda çıktısı verir
+    //get set olmazsa year değerini değiştiremezdik, çünkü year değişkeni private olarak tanımlanmıştır, sadece Car sınıfı içinden erişilebilir, dışarıdan erişilemez
+    // car1.year = 2021 
+    //println(car1.year) //Car.kt:54:14: error: cannot access 'var year: Int': it is private in 'com.example.kotlin.models.Car'.
+    println(car1.getYear()) // get >> 2020 çıktısı verir çünkü year değeri 2020 olarak atanmıştır
+    car1.setYear(2021)
+    println(car1.getYear()) 
+    car1.setYear(1980) // Invalid year çıktısı verir çünkü 1980 geçersiz bir yıl olarak kabul edilir
+    println(car1.getYear()) // get >> 2021 çıktısı verir çünkü year değeri 2021 olarak atanmıştır, 1980 geçersiz bir yıl olarak kabul edildiği için year değeri değişmemiştir
+   // car2.rent() // The car is rented  0 çıktısı verir çünkü brand, model ve year değerleri boş ve sıfır olarak atanmıştır
 }
 
